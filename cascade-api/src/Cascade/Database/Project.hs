fca3a57cd6c665fc5970250191c5529bb4bff304
Mohammad Hasani <the-dr-lazy@pm.me>
2021-01-04 21:10:23 +0330

    feat(cascade-api): add projects database model

---
+++ cascade-api/src/Cascade/Database/Project.hs
@@ -0,0 +1,39 @@
+module Cascade.Database.Project
+  ( ProjectTable
+  , PrimaryKey(..)
+  , Id
+  , Project
+  ) where
+
+import           Data.UUID                      ( UUID )
+import           Database.Beam                  ( Beamable
+                                                , C
+                                                , PrimaryKey
+                                                , Table
+                                                )
+import qualified Database.Beam                 as Beam
+
+-- brittany-disable-next-binding
+data ProjectTable (f :: Type -> Type) = Project
+  { id   :: C f UUID
+  , name :: C f Text
+  }
+  deriving stock Generic
+  deriving anyclass Beamable
+
+instance Table ProjectTable where
+  data PrimaryKey ProjectTable f = Id
+    { unId :: C f UUID
+    }
+    deriving stock Generic
+    deriving anyclass Beamable
+  primaryKey = Id . id
+
+
+type Id = PrimaryKey ProjectTable Identity
+
+
+type Project = ProjectTable Identity
+
+deriving stock instance Show Project
+deriving stock instance Eq Project
