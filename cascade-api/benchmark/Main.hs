3cb3d5c43ffcd0a2418d75909453275fc6d738ce
Mohammad Hasani <the-dr-lazy@pm.me>
2021-01-04 19:53:33 +0330

    feat(cascade-api): init

---
+++ cascade-api/benchmark/Main.hs
@@ -0,0 +1,8 @@
+module Main
+  ( main
+  ) where
+
+import           Gauge.Main
+
+main :: IO ()
+main = defaultMain [bench "const" (whnf const ())]
---
+++ cascade-api/test/Spec.hs
@@ -0,0 +1,6 @@
+module Main
+  ( main
+  ) where
+
+main :: IO ()
+main = putStrLn ("Test suite is not implemented" :: String)
