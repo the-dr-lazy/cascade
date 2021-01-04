f43fb5c0637528a665f949dd3bc7aa17a5ea64ce
Mohammad Hasani <the-dr-lazy@pm.me>
2021-01-04 21:10:23 +0330

    feat(cascade-api): add projects database model

---
+++ cascade-api/src/Cascade/Database.hs
@@ -0,0 +1,20 @@
+module Cascade.Database
+  ( Database
+  , database
+  ) where
+
+import           Cascade.Database.Project       ( ProjectTable )
+import           Database.Beam                  ( DatabaseSettings
+                                                , TableEntity
+                                                )
+import qualified Database.Beam                 as Beam
+
+-- brittany-disable-next-binding
+data Database (f :: Type -> Type) = Database
+  { projects :: f (TableEntity ProjectTable)
+  }
+  deriving stock Generic
+  deriving anyclass (Beam.Database backend)
+
+database :: DatabaseSettings backend Database
+database = Beam.defaultDbSettings
