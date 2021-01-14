ccfe8839914ed1e3df4011a23cdc62c445f83268
Mohammad Hasani <the-dr-lazy@pm.me>
2021-01-15 00:10:29 +0330

    chore: configure headroom

--- cascade-prelude/src/Prelude.hs
+++ cascade-prelude/src/Prelude.hs
@@ -1,3 +1,15 @@
+{-|
+Module      : Prelude
+Description : !!! INSERT MODULE SHORT DESCRIPTION !!!
+Copyright   : (c) 2020-2021 Cascade
+License     : MPL 2.0
+Maintainer  : Mohammad Hasani <the-dr-lazy@pm.me> (the-dr-lazy.github.io)
+Stability   : Stable
+Portability : POSIX
+
+!!! INSERT MODULE LONG DESCRIPTION !!!
+-}
+
 module Prelude
   ( module Data.Map.Strict
   , module Data.UUID
