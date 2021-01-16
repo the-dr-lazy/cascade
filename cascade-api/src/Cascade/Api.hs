ec1b0700556af3e7236a91224bc4766a6e6c7d46
Mohammad Hasani <the-dr-lazy@pm.me>
2021-01-17 01:37:59 +0330

    refactor(cascade-api): move database connection allocation mechanism out of Cascade.Api.main

--- cascade-api/src/Cascade/Api.hs
+++ cascade-api/src/Cascade/Api.hs
@@ -6,33 +6,21 @@ import qualified Cascade.Api.Effect.Database   as Database
 import qualified Cascade.Api.Effect.Database.Project
                                                as Database.Project
 import           Cascade.Api.Network.Wai.Application
-import           Data.Pool                      ( Pool
-                                                , createPool
-                                                )
 import qualified Database.PostgreSQL.Simple    as Postgres
 import qualified Network.Wai.Handler.Warp      as Warp
 import           Polysemy                       ( runFinal )
 import           Polysemy.Error                 ( errorToIOFinal )
 import           Polysemy.Final                 ( embedToFinal )
-import           Polysemy.Input                 ( runInputConst )
 import qualified Servant
 
-mkDatabaseConnectionPool :: IO (Pool Postgres.Connection)
-mkDatabaseConnectionPool = createPool acquire Postgres.close 1 10 10
- where
-  acquire = Postgres.connectPostgreSQL
-    "postgresql://cascade:cascade@postgresql:5432/cascade"
-
-main :: IO ()
-main = do
-  databaseConnectionPool <- mkDatabaseConnectionPool
+main :: (forall a . (Postgres.Connection -> IO a) -> IO a) -> IO ()
+main withDatabaseConnection = do
   Warp.run 3141 $ application
     ( Servant.Handler
     . ExceptT
     . runFinal
     . errorToIOFinal
     . embedToFinal
-    . runInputConst databaseConnectionPool
-    . Database.runPostgres Database.runSqlByPostgresPooled
+    . Database.runPostgres withDatabaseConnection
     . Database.Project.run
     )
