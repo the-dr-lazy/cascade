afc892f143e11e4ebb94d9246a3b9655317ae3b4
Mohammad Hasani <the-dr-lazy@pm.me>
2021-01-22 01:29:27 +0330

    test(cascade-api): don't depend on inherited cwd

---
+++ cascade-api/test/Cascade/Api/Test/FilePath.hs
@@ -0,0 +1,16 @@
+module Cascade.Api.Test.FilePath where
+
+import           System.Directory
+import           System.FilePath
+
+findFileUpward :: (FilePath -> Bool) -> FilePath -> IO (Maybe FilePath)
+findFileUpward f top = do
+  contents <- getDirectoryContents base
+  case find f contents of
+    Nothing | base == top -> pure Nothing
+            | otherwise   -> findFileUpward f base
+    Just path -> return (Just (base </> path))
+  where base = takeDirectory top
+
+findSqitchConfigFileUpward :: FilePath -> IO (Maybe FilePath)
+findSqitchConfigFileUpward = findFileUpward (== "sqitch.conf")
