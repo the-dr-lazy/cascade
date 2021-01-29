12b6f73a8b8e7a65cfb0d4f4ae94ea8eb194647f
Mohammad Hasani <the-dr-lazy@pm.me>
2021-01-29 13:55:59 +0330

    fix(cascade-api): use renamed types in generators

--- cascade-api/test/Cascade/Api/Hedgehog/Gen/Api/Project.hs
+++ cascade-api/test/Cascade/Api/Hedgehog/Gen/Api/Project.hs
@@ -23,16 +23,16 @@ import qualified Hedgehog.Range                as Range
 class ProjectGenerator (f :: Type -> Type) where
   project :: MonadGen m => m (f Project)
 
-instance ProjectGenerator Readable where
+instance ProjectGenerator Project.Readable where
   project = Project.Readable <$> id <*> name
    where
     id   = Gen.uuid |> fmap Project.Id
     name = Gen.text (Range.linear 8 32) Gen.alphaNum
 
-instance ProjectGenerator Creatable where
+instance ProjectGenerator Project.Creatable where
   project = Project.Creatable <$> name
     where name = Gen.text (Range.linear 8 32) Gen.alphaNum
 
-instance ProjectGenerator Updatable where
+instance ProjectGenerator Project.Updatable where
   project = Project.Updatable <$> name
     where name = Gen.maybe (Gen.text (Range.linear 8 32) Gen.alphaNum)
