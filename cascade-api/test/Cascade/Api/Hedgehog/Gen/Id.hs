the-dr-lazy/cascade

commit b8c5b9d859bde500af5a01d21ee7110b132a6ef1

refactor(cascade-api): make Id type overloaded over entity
* refactor(cascade-api): make Id type overloaded over entity

* fix(cascade-api): hide Ambiguous Id

* refactor(cascade-api): remove redundant imports

* test(cascade-api): add Id generator

* refactor(cascade-api): move Id type to separate module

cascade-api/src/Cascade/Api/Data/Id.hs
@@ -1,0 +1,18 @@
+module Cascade.Api.Data.Id
+  ( Id(..)
+  ) where
+
+import           Control.Lens.TH                ( makeWrapped )
+import           Data.Aeson                     ( FromJSON
+                                                , ToJSON
+                                                )
+import           Servant.API                    ( FromHttpApiData
+                                                , ToHttpApiData
+                                                )
+
+newtype Id (entity :: Type) = Id
+  { unId :: UUID }
+  deriving stock Generic
+  deriving newtype (Show, Eq, Ord, FromHttpApiData, ToHttpApiData, FromJSON, ToJSON)
+
+makeWrapped ''Id

cascade-api/test/Cascade/Api/Hedgehog/Gen/Id.hs
@@ -1,0 +1,10 @@
+module Cascade.Api.Hedgehog.Gen.Id
+  ( id
+  ) where
+
+import           Cascade.Api.Data.Id
+import qualified Cascade.Api.Hedgehog.Gen      as Gen
+import           Hedgehog
+
+id :: MonadGen m => m (Id entity)
+id = Id <$> Gen.uuid

cascade-api/src/Cascade/Api/Data/Project.hs
@@ -11,31 +11,23 @@ Portability : POSIX
 -}
 
 module Cascade.Api.Data.Project
-  ( Id(..)
-  , Project
+  ( Project
+  , Id
   , Readable(..)
   , Creatable(..)
   , Updatable(..)
   ) where
 
+import qualified Cascade.Api.Data.Id           as Data
 import           Cascade.Api.Data.Prelude
-import           Control.Lens.TH                ( makeWrapped )
 import           Data.Aeson                     ( FromJSON
                                                 , ToJSON
                                                 )
 import           Data.Generics.Labels           ( )
-import           Servant.API                    ( FromHttpApiData
-                                                , ToHttpApiData
-                                                )
-
-newtype Id = Id
-  { unId :: UUID }
-  deriving stock Generic
-  deriving newtype (Show, Eq, Ord, FromHttpApiData, ToHttpApiData, FromJSON, ToJSON)
-
-makeWrapped ''Id
 
 data Project
+
+type Id = Data.Id Project
 
 data instance Readable Project = ProjectR
   { id   :: Id

cascade-api/test/Cascade/Api/Hedgehog/Gen/Api/Project.hs
@@ -15,8 +15,7 @@ module Cascade.Api.Hedgehog.Gen.Api.Project
   ) where
 
 import           Cascade.Api.Data.Project
-import qualified Cascade.Api.Data.Project      as Project
-import qualified Cascade.Api.Hedgehog.Gen      as Gen
+import qualified Cascade.Api.Hedgehog.Gen.Id   as Gen
 import           Hedgehog
 import qualified Hedgehog.Gen                  as Gen
 import qualified Hedgehog.Range                as Range
@@ -25,10 +24,8 @@ class ProjectGenerator (f :: Type -> Type) where
   project :: MonadGen m => m (f Project)
 
 instance ProjectGenerator Readable where
-  project = ProjectR <$> id <*> name
-   where
-    id   = Gen.uuid |> fmap Project.Id
-    name = Gen.text (Range.linear 8 32) Gen.alphaNum
+  project = ProjectR <$> Gen.id <*> name
+    where name = Gen.text (Range.linear 8 32) Gen.alphaNum
 
 instance ProjectGenerator Creatable where
   project = ProjectC <$> name

cascade-api/test/Test/Cascade/Api/StateMachine.hs
@@ -17,9 +17,9 @@ module Test.Cascade.Api.StateMachine
 import qualified Cascade.Api
 import           Cascade.Api.Data.Project
 import qualified Cascade.Api.Data.Project      as Project
-import qualified Cascade.Api.Hedgehog.Gen      as Gen
 import qualified Cascade.Api.Hedgehog.Gen.Api.Project
                                                as Gen
+import qualified Cascade.Api.Hedgehog.Gen.Id   as Gen
 import qualified Cascade.Api.Network.TestClient.Api.Projects
                                                as Cascade.Api.Projects
 import qualified Cascade.Api.Servant.Response  as Response
@@ -181,8 +181,7 @@ c_addNotExistingId :: forall g m
                    => Applicative m => Command g m Model
 c_addNotExistingId =
   let generator :: Model Symbolic -> Maybe (g (AddNotExistingId Symbolic))
-      generator _ =
-        Gen.uuid |> fmap Project.Id |> fmap AddNotExistingId |> Just
+      generator _ = Gen.id |> fmap AddNotExistingId |> Just
 
       execute :: AddNotExistingId Concrete -> m Project.Id
       execute (AddNotExistingId id) = pure id

commit 5dba581434847a0f746f4f1400257354a63a454e

chore: run headroom on missing files
This commit is irrelevant to the PR.
The header problem introduced by #22 and the problem was permissive
GitHub repository settings about Headroom CI job.
The settings has been changed.

cascade-api/src/Cascade/Api/Data/Id.hs
@@ -1,3 +1,15 @@
+{-|
+Module      : Cascade.Api.Data.Id
+Description : !!! INSERT MODULE SHORT DESCRIPTION !!!
+Copyright   : (c) 2020-2021 Cascade
+License     : MPL 2.0
+Maintainer  : Mohammad Hasani <the-dr-lazy@pm.me> (the-dr-lazy.github.io)
+Stability   : Stable
+Portability : POSIX
+
+!!! INSERT MODULE LONG DESCRIPTION !!!
+-}
+
 module Cascade.Api.Data.Id
   ( Id(..)
   ) where

cascade-api/test/Cascade/Api/Hedgehog/Gen/Id.hs
@@ -1,3 +1,15 @@
+{-|
+Module      : Cascade.Api.Hedgehog.Gen.Id
+Description : !!! INSERT MODULE SHORT DESCRIPTION !!!
+Copyright   : (c) 2020-2021 Cascade
+License     : MPL 2.0
+Maintainer  : Mohammad Hasani <the-dr-lazy@pm.me> (the-dr-lazy.github.io)
+Stability   : Stable
+Portability : POSIX
+
+!!! INSERT MODULE LONG DESCRIPTION !!!
+-}
+
 module Cascade.Api.Hedgehog.Gen.Id
   ( id
   ) where