c4626c131e7dc735f03ebd8157ea7a47d33668d6
Mohammad Hasani <the-dr-lazy@pm.me>
2021-01-29 14:44:55 +0330

    fix(cascade-api): hide Ambiguous Id

--- cascade-api/src/Cascade/Api/Data/Project.hs
+++ cascade-api/src/Cascade/Api/Data/Project.hs
@@ -19,6 +19,7 @@ module Cascade.Api.Data.Project
   ) where
 
 import           Cascade.Api.Data.Prelude
+                                         hiding ( Id )
 import qualified Cascade.Api.Data.Prelude      as Prelude
 import           Control.Lens.TH                ( makeWrapped )
 import           Data.Aeson                     ( FromJSON
