0b9dbe6c7947f2cdfe14b475b15658ae8f31fbf7
Mohammad Hasani <the-dr-lazy@pm.me>
2021-02-05 21:53:17 +0330

    refactor(cascade-api): remove unused RawCredentialValidationErrors

--- cascade-api/src/Cascade/Api/Data/Authentication.hs
+++ cascade-api/src/Cascade/Api/Data/Authentication.hs
@@ -11,45 +11,28 @@ Portability : POSIX
 -}
 
 module Cascade.Api.Data.Authentication
-  ( RawCredentialV(..)
-  , RawCredential
-  , RawCredentialValidationErrors
+  ( RawCredential(..)
   , ParsedCredential
   , parseRawCredential
   ) where
 
 import qualified Cascade.Api.Data.ByteString.Password
                                                as Password
-import           Cascade.Api.Data.Prelude
 import qualified Cascade.Api.Data.Text.Username
                                                as Username
 import qualified Cascade.Api.Data.User         as User
 import           Data.Aeson                     ( FromJSON
                                                 , ToJSON
                                                 )
-import           Data.Monoid.Generic
 import           Validation
 
 -- brittany-disable-next-binding
-data RawCredentialV (f :: Type -> Type) = RawCredential
-  { username :: Validatable f Text (Maybe Username.ValidationErrors)
-  , password :: Validatable f Text (Maybe Password.ValidationErrors)
+data RawCredential = RawCredential
+  { username :: Text
+  , password :: Text
   }
-  deriving stock Generic
-
-type RawCredential = RawCredentialV Identity
-
-deriving stock instance Show RawCredential
-deriving stock instance Eq RawCredential
-deriving anyclass instance FromJSON RawCredential
-deriving anyclass instance ToJSON RawCredential
-
-type RawCredentialValidationErrors = RawCredentialV Validate
-
-deriving stock instance Show RawCredentialValidationErrors
-deriving anyclass instance ToJSON RawCredentialValidationErrors
-deriving via (GenericSemigroup RawCredentialValidationErrors) instance Semigroup RawCredentialValidationErrors
-deriving via (GenericMonoid RawCredentialValidationErrors) instance Monoid RawCredentialValidationErrors
+  deriving stock (Generic, Show, Eq)
+  deriving anyclass (FromJSON, ToJSON)
 
 data ParsedCredential = ParsedCredential
   { username :: User.Username
@@ -57,14 +40,8 @@ data ParsedCredential = ParsedCredential
   }
   deriving stock (Generic, Show, Eq)
 
-parseRawCredential :: RawCredential
-                   -> Validation
-                        RawCredentialValidationErrors
-                        ParsedCredential
+parseRawCredential :: RawCredential -> Validation () ParsedCredential
 parseRawCredential RawCredential {..} =
-  let validateUsername = Username.mk username |> first \e ->
-        mempty { username = Just e } :: RawCredentialValidationErrors
-      validatePassword =
-        password |> encodeUtf8 |> Password.mk |> first \e ->
-          mempty { password = Just e } :: RawCredentialValidationErrors
+  let validateUsername = Username.mk username |> first mempty
+      validatePassword = password |> encodeUtf8 |> Password.mk |> first mempty
   in  ParsedCredential <$> validateUsername <*> validatePassword
