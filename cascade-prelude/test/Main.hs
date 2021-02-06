74b6b0800c391db49dd297ac61b43313e99b2327
Mohammad Hasani <the-dr-lazy@pm.me>
2021-02-06 17:07:10 +0330

    test(cascade-prelude): init

---
+++ cascade-prelude/test/Main.hs
@@ -0,0 +1,19 @@
+{-|
+Module      : Main
+Description : !!! INSERT MODULE SHORT DESCRIPTION !!!
+Copyright   : (c) 2020-2021 Cascade
+License     : MPL 2.0
+Maintainer  : Mohammad Hasani <the-dr-lazy@pm.me> (the-dr-lazy.github.io)
+Stability   : Stable
+Portability : POSIX
+
+!!! INSERT MODULE LONG DESCRIPTION !!!
+-}
+
+module Main
+  ( main
+  ) where
+
+
+main :: IO ()
+main = putStrLn ("Test suite is not implemented" :: String)
