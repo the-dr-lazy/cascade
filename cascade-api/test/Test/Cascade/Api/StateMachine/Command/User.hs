28abb5dccea41e94689282039c7e9342c85475c7
Mohammad Hasani <the-dr-lazy@pm.me>
2021-02-07 15:50:22 +0330

    chore: run Headroom

--- cascade-api/test/Test/Cascade/Api/StateMachine/Command/User.hs
+++ cascade-api/test/Test/Cascade/Api/StateMachine/Command/User.hs
@@ -1,3 +1,15 @@
+{-|
+Module      : Test.Cascade.Api.StateMachine.Command.User
+Description : !!! INSERT MODULE SHORT DESCRIPTION !!!
+Copyright   : (c) 2020-2021 Cascade
+License     : MPL 2.0
+Maintainer  : Mohammad Hasani <the-dr-lazy@pm.me> (the-dr-lazy.github.io)
+Stability   : Stable
+Portability : POSIX
+
+!!! INSERT MODULE LONG DESCRIPTION !!!
+-}
+
 module Test.Cascade.Api.StateMachine.Command.User
   ( commands
   ) where
