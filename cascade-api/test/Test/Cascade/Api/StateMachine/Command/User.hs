fd0b52a8e790bedb1183035d6cec27d5170067a5
Mohammad Hasani <the-dr-lazy@pm.me>
2021-02-07 15:32:49 +0330

    test(cascade-api): create existing user

--- cascade-api/test/Test/Cascade/Api/StateMachine/Command/User.hs
+++ cascade-api/test/Test/Cascade/Api/StateMachine/Command/User.hs
@@ -26,7 +26,7 @@ import           Test.Cascade.Api.StateMachine.Model
 commands :: MonadGen g
          => GenBase g ~ Identity
          => MonadIO m => MonadTest m => [Command g m Model]
-commands = [createNotExisting]
+commands = [createNotExisting, createExisting]
 
 -- brittany-disable-next-binding
 data CreateNotExisting (v :: Type -> Type) = CreateNotExisting
@@ -114,4 +114,52 @@ createNotExisting =
         CreateNotExisting Valid   _ -> statusCode === 201
         CreateNotExisting Invalid _ -> statusCode === 422
   in
-    Command generator execute [Update update, Ensure ensure]
+    Command generator execute [Require require, Update update, Ensure ensure]
+
+-- brittany-disable-next-binding
+newtype CreateExisting (v :: Type -> Type) = CreateExisting
+  { creatable :: User.RawCreatable }
+  deriving stock Show
+
+instance HTraversable CreateExisting where
+  htraverse _ input = pure $ coerce input
+
+createExisting :: forall g m
+                . MonadGen g
+               => MonadIO m => MonadTest m => Command g m Model
+createExisting =
+  let generator :: Model Symbolic -> Maybe (g (CreateExisting Symbolic))
+      generator model | null usernames || null emailAddresses = Nothing
+                      | otherwise = creatable |> fmap CreateExisting |> Just
+         where
+          usernames      = model ^. #user . #byUsername . to Map.keys
+          emailAddresses = model ^. #user . #byEmailAddress . to Map.keys
+          creatable =
+            User.RawCreatable
+              <$> Gen.element usernames
+              <*> Gen.element emailAddresses
+              <*> Gen.password Valid
+
+      execute :: CreateExisting Concrete -> m Cascade.Api.Users.CreateResponse
+      execute (CreateExisting creatable) = do
+        label "[User/Create Existing]"
+        evalIO $ Cascade.Api.Users.create creatable
+
+      require :: Model Symbolic -> CreateExisting Symbolic -> Bool
+      require model (CreateExisting creatable) =
+        doesUsernameExist && doesEmailAddressExist
+         where
+          doesUsernameExist =
+            Map.member (creatable ^. #username) (model ^. #user . #byUsername)
+          doesEmailAddressExist = Map.member (creatable ^. #emailAddress)
+                                             (model ^. #user . #byEmailAddress)
+
+      ensure :: Model Concrete
+             -> Model Concrete
+             -> CreateExisting Concrete
+             -> Cascade.Api.Users.CreateResponse
+             -> Test ()
+      ensure _ _ _ response = do
+        footnoteShow response
+        response ^. #responseStatusCode . #statusCode === 409
+  in  Command generator execute [Require require, Ensure ensure]
