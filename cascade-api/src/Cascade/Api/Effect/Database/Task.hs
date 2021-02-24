a4afe49e488650efb2673b63112821b53265d064
Mahdi Seyedan <seyedmahdiseydan78@yahoo.com>
2021-02-24 04:13:39 +0330

    chore: run headroom

--- cascade-api/src/Cascade/Api/Effect/Database/Task.hs
+++ cascade-api/src/Cascade/Api/Effect/Database/Task.hs
@@ -1,3 +1,15 @@
+{-|
+Module      : Cascade.Api.Effect.Database.Task
+Description : !!! INSERT MODULE SHORT DESCRIPTION !!!
+Copyright   : (c) 2020-2021 Cascade
+License     : MPL 2.0
+Maintainer  : Mohammad Hasani <the-dr-lazy@pm.me> (the-dr-lazy.github.io)
+Stability   : Stable
+Portability : POSIX
+
+!!! INSERT MODULE LONG DESCRIPTION !!!
+-}
+
 module Cascade.Api.Effect.Database.Task
   ( TaskL(..)
   , findByProjectId
