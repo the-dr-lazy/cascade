c9af7b4a9d22457ebb3c2ccd1e556c314973cdf6
Mahdi Seyedan <seyedmahdiseydan78@yahoo.com>
2021-02-25 15:26:11 +0330

    fix(cascade-api): handle non existing project for creating task

--- cascade-api/src/Cascade/Api/Network/Server/Api/Projects/Tasks.hs
+++ cascade-api/src/Cascade/Api/Network/Server/Api/Projects/Tasks.hs
@@ -19,16 +19,23 @@ import qualified Cascade.Api.Data.Task         as Task
 import qualified Cascade.Api.Data.Project      as Project
 import qualified Cascade.Api.Effect.Database.Task
                                                as Database.Task
+import qualified Cascade.Api.Effect.Database.Project
+                                               as Database.Project
 import           Cascade.Api.Effect.Database.Task
                                                 ( TaskL )
-import           Cascade.Api.Effect.Time        ( TimeL
-                                                , now
-                                                )
+import           Cascade.Api.Effect.Database.Project
+                                                ( ProjectL )
+import qualified Cascade.Api.Effect.Time       as Time
+import           Cascade.Api.Effect.Time        ( TimeL )
 import           Cascade.Api.Network.Anatomy.Api.Projects.Tasks
 import qualified Cascade.Api.Servant.Response  as Response
 import           Polysemy                       ( Member
                                                 , Members
                                                 , Sem
+                                                )
+
+import           Polysemy.Error                 ( Error
+                                                , throw
                                                 )
 import           Servant
 import           Servant.API.Generic
@@ -38,23 +45,25 @@ import           Servant.Server.Generic         ( AsServerT
 import           Validation                     ( validation )
 
 handleCreate
-  :: Members '[TaskL, TimeL] r
+  :: Members '[TaskL, ProjectL, TimeL] r
   => Project.Id
   -> Task.RawCreatable
   -> Sem r (Union CreateResponse)
 handleCreate projectId creatable =
-  now
+  Time.now
     >>= validation (respond . Response.Unprocessable) go
     .   Task.parseRawCreatableTask creatable
  where
   go
-    :: Members '[TaskL, TimeL] r
+    :: Members '[TaskL, ProjectL, TimeL] r
     => Task.ParsedCreatable
     -> Sem r (Union CreateResponse)
-  go parsedCreatable =
-    Database.Task.create parsedCreatable projectId
-      >>= respond
-      .   Response.created
+  go parsedCreatable = Database.Project.findById projectId >>= maybe
+    (respond Response.notFound)
+    \_ ->
+      Database.Task.create parsedCreatable projectId
+        >>= respond
+        .   Response.created
 
 handleGetByProjectId
   :: Member TaskL r => Project.Id -> Sem r (Union GetByProjectIdResponse)
@@ -62,7 +71,7 @@ handleGetByProjectId projectId =
   Database.Task.findByProjectId projectId >>= respond . Response.ok
 
 server
-  :: Members '[TaskL, TimeL] r
+  :: Members '[TaskL, ProjectL, TimeL] r
   => Project.Id
   -> ToServant Routes (AsServerT (Sem r))
 server projectId = genericServerT Routes
