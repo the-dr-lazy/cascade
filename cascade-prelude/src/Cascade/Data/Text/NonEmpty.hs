630a65bc5bc278e384eadb8c294c4af9a63a8114
Mahdi Seyedan <seyedmahdiseydan78@yahoo.com>
2021-03-11 01:10:32 +0330

    feat(cascade-prelude): implement Validatable instance for Text.NonEmpty

--- cascade-prelude/src/Cascade/Data/Text/NonEmpty.hs
+++ cascade-prelude/src/Cascade/Data/Text/NonEmpty.hs
@@ -10,10 +10,16 @@ Portability : POSIX
 !!! INSERT MODULE LONG DESCRIPTION !!!
 -}
 
-module Cascade.Data.Text.NonEmpty (NonEmpty, pattern NonEmpty, un, mk) where
+{-# LANGUAGE DeriveDataTypeable #-}
+
+module Cascade.Data.Text.NonEmpty (NonEmpty, pattern NonEmpty, un, mk, ValidationError(..)) where
 
 
 import           Prelude                      hiding ( NonEmpty )
+import qualified Prelude
+import           Cascade.Data.Validation
+import qualified Cascade.Data.Validation            as Validation
+import           Data.Data                           ( Data )
 import           Control.Lens.TH                     ( makeWrapped )
 import           Data.Aeson                          ( FromJSON
                                                      , ToJSON
@@ -32,3 +38,17 @@ pattern NonEmpty a <- Mk a
 
 mk :: Text -> Maybe NonEmpty
 mk input = if Text.null input then Nothing else Just $ Mk input
+
+data ValidationError = IsEmpty
+  deriving stock (Generic, Data, Show)
+  deriving ToJSON via (ApiErrorFormat ValidationError)
+
+type ValidationErrors = Prelude.NonEmpty ValidationError
+
+instance Validation.ToMessage ValidationError where
+  toMessage IsEmpty = "empty text"
+
+instance Validatable Text NonEmpty where
+  type Errors Text NonEmpty = ValidationErrors
+
+  parse = pure . maybeToSuccess (IsEmpty :| []) . mk
