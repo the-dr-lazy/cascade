6f748264d9f66a64b742abdf34cbee3adb94e17a
Mahdi Seyedan <seyedmahdiseydan78@yahoo.com>
2021-03-11 01:11:46 +0330

    feat(cascade-api): implement Validatable instance for Deadline

--- cascade-api/src/Cascade/Api/Data/OffsetDatetime/Deadline.hs
+++ cascade-api/src/Cascade/Api/Data/OffsetDatetime/Deadline.hs
@@ -12,7 +12,52 @@ Portability : POSIX
 
 module Cascade.Api.Data.OffsetDatetime.Deadline (Deadline, un, mk) where
 
-import           Cascade.Data.Chronos.Future
-import           Chronos                             ( OffsetDatetime )
+import           Cascade.Data.Chronos.Future         ( Future )
+import qualified Cascade.Data.Chronos.Future        as Future
+import           Chronos                             ( OffsetDatetime
+                                                     , Time
+                                                     )
+import           Data.Data                           ( Data )
+import           Cascade.Api.Data.OffsetDatetime     ( FormattedOffsetDatetime
+                                                     , unFormattedOffsetDatetime
+                                                     )
+import           Cascade.Data.Validation
+import qualified Cascade.Data.Validation            as Validation
+import           Data.Aeson                          ( FromJSON
+                                                     , ToJSON
+                                                     )
+import           Cascade.Api.Effect.Time             ( TimeL )
+import qualified Cascade.Api.Effect.Time            as Time
 
-type Deadline = Future OffsetDatetime
+newtype Deadline = Deadline (Future OffsetDatetime)
+
+un :: Deadline -> OffsetDatetime
+un (Deadline future) = Future.un future
+
+mk :: OffsetDatetime -> Time -> Maybe Deadline
+mk date now = Deadline <$> Future.mk date now
+
+data ValidationError = IsPast
+  deriving stock (Generic, Data, Show)
+  deriving ToJSON via (ApiErrorFormat ValidationError)
+
+instance Validation.ToMessage ValidationError where
+  toMessage IsPast = "past date"
+
+type ValidationErrors = NonEmpty ValidationError
+
+instance Validatable OffsetDatetime Deadline where
+  type Errors OffsetDatetime Deadline = ValidationErrors
+  type Effects OffsetDatetime Deadline = '[TimeL]
+
+  parse date = do
+    now <- Time.now
+    pure . maybeToSuccess (IsPast :| []) $ mk date now
+
+instance Validatable FormattedOffsetDatetime Deadline where
+  type Errors FormattedOffsetDatetime Deadline = ValidationErrors
+  type Effects FormattedOffsetDatetime Deadline = '[TimeL]
+
+  parse date = do
+    now <- Time.now
+    pure . maybeToSuccess (IsPast :| []) $ mk (unFormattedOffsetDatetime date) now
