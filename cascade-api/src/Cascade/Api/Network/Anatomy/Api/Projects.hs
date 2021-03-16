9c673270a2c4962fea08ef0d3f5138b6b07c483a
Mohammad Hasani <the-dr-lazy@pm.me>
2021-03-16 21:51:31 +0330

    fix(cascade-api): remove forbidden response from projects endpoint

--- cascade-api/src/Cascade/Api/Network/Anatomy/Api/Projects.hs
+++ cascade-api/src/Cascade/Api/Network/Anatomy/Api/Projects.hs
@@ -19,11 +19,11 @@ import           Cascade.Api.Network.Anatomy.Prelude
 import qualified Cascade.Api.Servant.Response       as Response
 import           Data.Generics.Labels                ( )
 
-type GetByIdResponse = '[Response.Unauthorized , Response.Forbidden , Response.NotFound , Response.Ok Project.Readable]
+type GetByIdResponse = '[Response.Unauthorized , Response.NotFound , Response.Ok Project.Readable]
 
-type UpdateByIdResponse = '[Response.Unauthorized , Response.Forbidden , Response.NotFound , Response.Ok Project.Readable]
+type UpdateByIdResponse = '[Response.Unauthorized , Response.NotFound , Response.Ok Project.Readable]
 
-type DeleteByIdResponse = '[Response.Unauthorized , Response.Forbidden , Response.NotFound , Response.Ok Project.Readable]
+type DeleteByIdResponse = '[Response.Unauthorized , Response.NotFound , Response.Ok Project.Readable]
 
 data Routes route = Routes
   { deleteById :: route :- Capture "id" Project.Id :> Auth :> Delete '[JSON] DeleteByIdResponse
