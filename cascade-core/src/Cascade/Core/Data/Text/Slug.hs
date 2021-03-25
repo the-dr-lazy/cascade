2d41993d1c4e06d210c029afd1d243e163488197
Mohammad Hasani <the-dr-lazy@pm.me>
2021-03-25 19:51:39 +0430

    feat(cascade-core): move Slug type into internal modules

--- cascade-core/src/Cascade/Core/Data/Text/Slug.hs
+++ cascade-core/src/Cascade/Core/Data/Text/Slug.hs
@@ -10,39 +10,7 @@ Portability : POSIX
 !!! INSERT MODULE LONG DESCRIPTION !!!
 -}
 
-module Cascade.Core.Data.Text.Slug (Slug, pattern Slug, mk, un) where
+module Cascade.Core.Data.Text.Slug (module Cascade.Core.Internal.Data.Text.Slug) where
 
-import           Cascade.Core.Data.Phase             ( Phase )
-import qualified Cascade.Core.Data.Phase            as Phase
-import qualified Cascade.Data.Char                  as Char
-import qualified Data.Text                          as Text
-import qualified Validation
-import           Validation                          ( Validation )
-
--- brittany-disable-next-binding
-newtype Slug (phase :: Phase) = Mk { un :: Text }
-
-pattern Slug :: Text -> Slug phase
-pattern Slug a <- Mk a
-
-data Error
-  = InvalidLengthError Word
-  | InvalidCharactersError
-  | DuplicateDashesError
-  | LeadingDashesError
-  | TrailingDashesError
-  deriving stock Show
-
-type Errors = NonEmpty Error
-
-mk :: Text -> Validation Errors (Slug 'Phase.Unknown)
-mk (Text.toLower . Text.strip -> input) = Mk input <$ validate input
-
-validate :: Text -> Validation Errors ()
-validate input = Validation.failureIf (Text.null input) (InvalidLengthError 0) `Validation.whenFailure_` const
-  (  Validation.failureIf (l > 233) (InvalidLengthError l)
-  *> Validation.failureIf ("-" `Text.isPrefixOf` input) LeadingDashesError
-  *> Validation.failureIf ("-" `Text.isPrefixOf` input) TrailingDashesError
-  *> Validation.failureIf (not . Text.all Char.isAlphaNumDash <| input) InvalidCharactersError
-  )
-  where l = fromIntegral . Text.length <| input
+import           Cascade.Core.Internal.Data.Text.Slug
+                                              hiding ( unsafePhaseCoerce )
---
+++ cascade-core/src/Cascade/Core/Internal/Data/Text/Slug.hs
@@ -0,0 +1,51 @@
+{-|
+Module      : Cascade.Core.Internal.Data.Text.Slug
+Description : !!! INSERT MODULE SHORT DESCRIPTION !!!
+Copyright   : (c) 2020-2021 Cascade
+License     : MPL 2.0
+Maintainer  : Mohammad Hasani <the-dr-lazy@pm.me> (the-dr-lazy.github.io)
+Stability   : Stable
+Portability : POSIX
+
+!!! INSERT MODULE LONG DESCRIPTION !!!
+-}
+
+module Cascade.Core.Internal.Data.Text.Slug (Slug, pattern Slug, mk, un, unsafePhaseCoerce) where
+
+import           Cascade.Core.Data.Phase             ( Phase )
+import qualified Cascade.Core.Data.Phase            as Phase
+import qualified Cascade.Data.Char                  as Char
+import qualified Data.Text                          as Text
+import qualified Validation
+import           Validation                          ( Validation )
+
+-- brittany-disable-next-binding
+newtype Slug (phase :: Phase) = Mk { un :: Text }
+
+pattern Slug :: Text -> Slug phase
+pattern Slug a <- Mk a
+
+data Error
+  = InvalidLengthError Word
+  | InvalidCharactersError
+  | DuplicateDashesError
+  | LeadingDashesError
+  | TrailingDashesError
+  deriving stock Show
+
+type Errors = NonEmpty Error
+
+mk :: Text -> Validation Errors (Slug 'Phase.Unknown)
+mk (Text.toLower . Text.strip -> input) = Mk input <$ validate input
+
+validate :: Text -> Validation Errors ()
+validate input = Validation.failureIf (Text.null input) (InvalidLengthError 0) `Validation.whenFailure_` const
+  (  Validation.failureIf (l > 233) (InvalidLengthError l)
+  *> Validation.failureIf ("-" `Text.isPrefixOf` input) LeadingDashesError
+  *> Validation.failureIf ("-" `Text.isPrefixOf` input) TrailingDashesError
+  *> Validation.failureIf (not . Text.all Char.isAlphaNumDash <| input) InvalidCharactersError
+  )
+  where l = fromIntegral . Text.length <| input
+
+unsafePhaseCoerce :: Slug p -> Slug p'
+unsafePhaseCoerce = coerce
