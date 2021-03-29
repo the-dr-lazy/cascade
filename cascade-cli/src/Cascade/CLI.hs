7edb7f3337650662a3c6171f8dd318a9c74b9627
Mahdi Seyedan <seyedmahdiseydan78@yahoo.com>
2021-03-30 01:54:48 +0430

    refactor(cascade-cli): seprate function for postgres options and fix tests

--- cascade-cli/src/Cascade/CLI.hs
+++ cascade-cli/src/Cascade/CLI.hs
@@ -6,9 +6,7 @@ import           Data.Pool                           ( Pool
                                                      , createPool
                                                      )
 import qualified Database.PostgreSQL.Simple         as Postgres
-import           Data.Version                        ( Version
-                                                     , showVersion
-                                                     )
+import           Data.Version                        ( showVersion )
 import qualified Paths_cascade_cli                  as Meta
                                                      ( version )
 import           Options.Applicative                 ( Parser
@@ -37,36 +35,32 @@ mkDatabaseConnectionPool :: PostgresOptions -> IO (Pool Postgres.Connection)
 mkDatabaseConnectionPool PostgresOptions {..} = do
   createPool acquire Postgres.close 1 10 10
  where
-  connectionInfo = Postgres.ConnectInfo { connectHost     = postgresHost
-                                        , connectPort     = postgresPort
-                                        , connectUser     = postgresUser
-                                        , connectPassword = postgresPassword
-                                        , connectDatabase = postgresDatabase
-                                        }
+  connectionInfo =
+    Postgres.ConnectInfo { connectHost = host, connectPort = port, connectUser = user, connectPassword = password, connectDatabase = database }
   acquire = Postgres.connect connectionInfo
 
 runCascadeApi :: Options -> IO ()
 runCascadeApi Options {..} = do
   databaseConnectionPool <- mkDatabaseConnectionPool postgresOptions
-  Cascade.Api.main <| Cascade.Api.Config { port = httpPort, withDatabaseConnection = Pool.withResource databaseConnectionPool }
+  Cascade.Api.main Cascade.Api.Config { port = httpPort, withDatabaseConnection = Pool.withResource databaseConnectionPool }
 
-cascadeVersion :: Version -> String
-cascadeVersion version = intercalate "\n" [cVersion, cHash, cDate]
+cascadeVersion :: String
+cascadeVersion = intercalate "\n" [cVersion, cHash, cDate]
  where
   cVersion, cHash, cDate :: String
-  cVersion = "Cascade CLI v" <> showVersion version
+  cVersion = "Cascade CLI v" <> showVersion Meta.version
   cHash    = "Git revision: " <> $(gitHash)
   cDate    = "Last commit: " <> $(gitCommitDate)
 
-versionP :: Version -> Parser (a -> a)
-versionP version = infoOption (cascadeVersion version) <| mconcat [long "version", short 'v', help "Show cascade's version"]
+versionP :: Parser (a -> a)
+versionP = infoOption cascadeVersion <| mconcat [long "version", short 'v', help "Show cascade's version"]
 
 data PostgresOptions = PostgresOptions
-  { postgresHost     :: !String
-  , postgresPort     :: !Word16
-  , postgresUser     :: !String
-  , postgresPassword :: !String
-  , postgresDatabase :: !String
+  { host     :: !String
+  , port     :: !Word16
+  , user     :: !String
+  , password :: !String
+  , database :: !String
   }
 
 data Options = Options
@@ -74,26 +68,25 @@ data Options = Options
   , postgresOptions :: PostgresOptions
   }
 
+postgresOptionsP :: Parser PostgresOptions
+postgresOptionsP =
+  PostgresOptions
+    <$> strOption (mconcat [long "postgres-host", metavar "CASCADE_POSTGRES_HOST", value "localhost", showDefault, help "PostgreSQL host"])
+    <*> option auto (mconcat [long "postgres-port", metavar "CASCADE_POSTGRES_PORT", value 5432, showDefault, help "PostgresSQL port"])
+    <*> strOption (mconcat [long "postgres-user", metavar "CASCADE_POSTGRES_USER", value "cascade", showDefault, help "PostgreSQL user"])
+    <*> strOption (mconcat [long "postgres-password", metavar "CASCADE_POSTGRES_PASSWORD", value "", showDefault, help "PostgreSQL passowrd"])
+    <*> strOption
+          (mconcat [long "postgres-database", metavar "CASCADE_POSTGRES_DATABASE", value "cascade-api", showDefault, help "PostgreSQL database"]
+          )
+
 optionsP :: Parser Options
 optionsP =
   Options
     <$> option auto (mconcat [long "http-port", metavar "CASCADE_HTTP_PORT", value 3141, showDefault, help "Port number of Cascade Api"])
-    <*> postgresOpts
- where
-  postgresOpts :: Parser PostgresOptions
-  postgresOpts =
-    PostgresOptions
-      <$> strOption (mconcat [long "postgres-host", metavar "CASCADE_POSTGRES_HOST", value "localhost", showDefault, help "PostgreSQL host"])
-      <*> option auto (mconcat [long "postgres-port", metavar "CASCADE_POSTGRES_PORT", value 5432, showDefault, help "Postgres port"])
-      <*> strOption (mconcat [long "postgres-user", metavar "CASCADE_POSTGRES_USER", value "cascade", showDefault, help "PostgreSQL user"])
-      <*> strOption (mconcat [long "postgres-password", metavar "CASCADE_POSTGRES_PASSWORD", value "", showDefault, help "PostgreSQL passowrd"])
-      <*> strOption
-            (mconcat
-              [long "postgres-database", metavar "CASCADE_POSTGRES_DATABASE", value "cascade-api", showDefault, help "PostgreSQL database"]
-            )
+    <*> postgresOptionsP
 
-cliParser :: Version -> ParserInfo Options
-cliParser version = info (helper <*> versionP version <*> optionsP) <| fullDesc <> progDesc "Cascade Cli"
+cliParser :: ParserInfo Options
+cliParser = info (helper <*> versionP <*> optionsP) <| fullDesc <> progDesc "Cascade Cli"
 
 main :: IO ()
-main = execParser (cliParser Meta.version) >>= runCascadeApi
+main = execParser cliParser >>= runCascadeApi
