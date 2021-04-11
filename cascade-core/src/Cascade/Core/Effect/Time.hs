5dd002d49c465a35d661df9b95a15c9a142c4c37
Mohammad Hasani <the-dr-lazy@pm.me>
2021-04-11 13:00:59 +0430

    feat(cascade-core): move Time effect module

--- cascade-core/src/Cascade/Core/Effect/Time.hs
+++ cascade-core/src/Cascade/Core/Effect/Time.hs
@@ -1,5 +1,5 @@
 {-|
-Module      : Cascade.Api.Effect.Time
+Module      : Cascade.Core.Effect.Time
 Description : !!! INSERT MODULE SHORT DESCRIPTION !!!
 Copyright   : (c) 2020-2021 Cascade
 License     : MPL 2.0
@@ -10,18 +10,12 @@ Portability : POSIX
 !!! INSERT MODULE LONG DESCRIPTION !!!
 -}
 
-module Cascade.Api.Effect.Time (TimeL, now, run) where
+module Cascade.Core.Effect.Time (TimeL, now, run) where
 
 
 import           Chronos                             ( Time )
 import qualified Chronos
-import           Polysemy                            ( Embed
-                                                     , Member
-                                                     , Sem
-                                                     , embed
-                                                     , interpret
-                                                     , makeSem
-                                                     )
+import           Polysemy                     hiding ( run )
 
 data TimeL (m :: Type -> Type) (a :: Type) where
   Now ::TimeL m Time
