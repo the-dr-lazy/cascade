d839a7ccdcc6c8135211ff5e65e1d44ccc60dcc5
Mohammad Hasani <the-dr-lazy@pm.me>
2021-07-19 19:51:32 +0430

    chore: add shell.nix

---
+++ shell.nix
@@ -0,0 +1,32 @@
+{ pkgs ? import ./nix { }, compiler ? "ghc884" }:
+
+let
+  haskellPackages = pkgs.haskell.packages.${compiler}.override {
+    overrides = final: prev: { haskeline = final.haskeline_0_8_1_2; };
+  };
+
+in haskellPackages.shellFor {
+  packages = _: [ ];
+  buildInputs = with pkgs; [
+    # Libraries:
+    zlib.dev
+    libjwt
+    postgresql_13
+
+    # Dev tools:
+    gitFull
+    niv
+    headroom
+    cabal-install
+    haskell-language-server
+    dhall
+    dhall-lsp-server
+    (pkgs.haskell.lib.justStaticExecutables haskellPackages.hpack-dhall)
+    (pkgs.haskell.lib.justStaticExecutables haskellPackages.stan)
+    ghcid
+    hlint
+    stylish-haskell
+    nixpkgs-fmt
+    sqitchPg
+  ];
+}
