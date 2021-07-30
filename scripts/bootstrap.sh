ec49fd088c3d9450049d8770b145267e27228d81
Mohammad Hasani <the-dr-lazy@pm.me>
2021-07-30 20:53:31 +0430

    chore: add OS X dev agent

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,8 +1,25 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-for file in $(git ls-files -cmo | grep -E "cascade-\w+/package.dhall\$"); do
-	dhall-hpack-cabal --package-dhall "$file"
-done
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+ROOT_DIR=$(dirname "$SCRIPTS_DIR")
+SERVICES_DIR="$ROOT_DIR/services"
 
 [[ "${CI:-false}" == "true" ]] || pre-commit install
+
+"$SCRIPTS_DIR/cabal.sh"
+
+case "$(uname)" in
+    'Darwin')
+        SERVICE_NAME="com.cascade.dev.agent.plist"
+        SERVICE_PATH="$HOME/Library/LaunchAgents/$SERVICE_NAME"
+        sed "s|<!--CASCADE_ROOT-->|$ROOT_DIR|g" "$SERVICES_DIR/osx/$SERVICE_NAME" > "$SERVICE_PATH"
+
+        if launchctl list | grep -q com.cascade.dev.agent; then
+            launchctl unload "$SERVICE_PATH"
+        fi
+
+        launchctl load "$SERVICE_PATH"
+        printf "Cascade develpment agent has been loaded"
+        ;;
+esac
--- scripts/watched-cabal.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-while sleep 1; do
-    git ls-files -cmo | grep -E ".hs|.dhall" | entr -cdr ./scripts/cabal.sh
-done
